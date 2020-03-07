930b66f3f8482cc47cb632dee43ea2dc5d382cc0
ignatius <fan01856472@gmail.com>
2020-03-07 04:29:58 -0800

    feature/add-login-and-signup

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="restaurant-forum">
+    <Navbar/>
+    <main 
+      role="main"
+      class="mt-5 bg white"
+    >
     <router-view/>
+    </main>
   </div>
 </template>
+
 <script>
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar.vue"
+
 export default {
-  name: 'App'
+  components: {
+    Navbar
+  }
 }
 </script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
