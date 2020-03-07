788dab1dd880f1fa75fc415a550822ea780a7c50
ignatius <fan01856472@gmail.com>
2020-03-07 04:59:41 -0800

    feature/handle-rest-routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SignIn from '../views/SignIn.vue'
-import SignUp from '../views/SignUp.vue'
+import Restaurants from '../views/Restaurants.vue'
 import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -15,7 +20,27 @@ const routes = [
   {
     path: '/signup',
     name: 'sign-up',
-    component: SignUp
+    component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/top',
+    name: 'top-restaurants',
+    component: () => import('../views/TopRestaurants.vue')
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurant-feeds',
+    component: () => import('../views/Feeds.vue')
+  },
+  {
+    path: '/users/popular',
+    name: 'popular-writers',
+    component: () => import('../views/PopularWriters.vue')
   },
   {
     path: '*',
