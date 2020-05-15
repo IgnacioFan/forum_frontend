42bb36d474652e7f3eff2df85a8cdbdead9fe6a9
ignatius <fan01856472@gmail.com>
2020-05-15 06:13:43 -0700

    (update feature) add users api

---
+++ src/apis/users.js
@@ -0,0 +1,40 @@
+import {apiHelper} from '../utils/helpers';
+const getToken = () => localStorage.getItem('token');
+
+export default {
+  addFavorite({restaurantId}) {
+    return apiHelper.post(`/favorite/${restaurantId}`, null, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  removeFavorite({restaurantId}) {
+    return apiHelper.delete(`/favorite/${restaurantId}`, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  addLike({restaurantId}) {
+    return apiHelper.post(`/like/${restaurantId}`, null, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  removeLike({restaurantId}) {
+    return apiHelper.delete(`/like/${restaurantId}`, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  getTopWriters(){
+    return apiHelper.get('/users/top', {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  addFollowing({userId}) {
+    return apiHelper.post(`/following/${userId}`, null, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+  removeFollowing({userId}) {
+    return apiHelper.delete(`/following/${userId}`, {
+      headers: { Authorization: `Bearer ${getToken()}`}
+    })
+  },
+}
